5b7a1664ccbe966358dabd2b2dcbcd73fbee5cda
Awais <awais381lead@gmail.com>
2022-08-27 10:26:44 -0700

    optimized the web app
    add functions for re-rendering reduction
    
    add comments in product details and app.js

---
+++ frontend/src/components/Others/Colors.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./color.css"
+const Colors = ({s,modes}) => {
+    // console.log(props.s)
+  return (
+    <>
+    <div className='colDiv' style={{background:`${s}`, color:`${s}`}}>
+    ""
+    </div>
+    </>
+  )
+}
+
+export default Colors
---
+++ frontend/src/components/layout/Header/LowerCatNav.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+// import { useAlert } from "react-alert";
+import { useSelector } from "react-redux";
+import AAsOfLowNav from "./AAsOfLowNav";
+
+const LowerCatNav = () => {
+  const { products } = useSelector((state) => state.products);
+  const { modes } = useSelector((state) => state.DarkMode);
+  const { isAuthenticated } = useSelector((state) => state.user);
+  const { prodi } = useSelector((state) => state.prods);
+
+  let arr = products.map((s) => s.category).reverse();
+
+  let f_arr = [...new Set(arr)];
+
+  f_arr.unshift("All_Products")
+  let arrayUniqueByKey = f_arr
+
+  let dark = {
+    bg: "black",
+    color: "black",
+    lbg: "grey",
+  };
+  let light = {
+    bg: "white",
+    // bg: "#f8f9fb",
+    color: "white",
+    lbg: "grey",
+  };
+  const [nav, setnav] = useState({
+    scroll: false,
+  });
+  const NavbarFixing = () => {
+    if (window.scrollY >= 4) {
+      setnav({
+        scroll: true,
+      });
+    } else {
+      setnav({
+        scroll: false,
+      });
+    }
+  };
+  window.addEventListener("scroll", NavbarFixing);
+
+  return (
+    <>
+      <div
+        id="container3"
+        className={`${
+          nav.scroll
+            ? `container3 ${isAuthenticated ? `posFix` : `posFix2`}`
+            : `container3`
+        }`}
+        style={{ backgroundColor: `${modes ? dark.bg : light.bg}` }}
+      >
+        {arrayUniqueByKey &&
+          arrayUniqueByKey.map((arrayUniqueByKeys) => (
+            <AAsOfLowNav
+              key={Math.random() * 1000}
+              product={arrayUniqueByKeys}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default LowerCatNav;
